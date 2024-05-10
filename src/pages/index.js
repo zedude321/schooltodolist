d8a844cfc0f6df77025138875b206de3fdd21309
Zedude321 <55306539+zedude321@users.noreply.github.com>
2024-05-10 20:58:08 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,3 @@
-import CreateTeam from "@/components/create-team";
-import CreateTask from "@/components/create-task";
-
 export default function Home() {
-  return(
-    <div className="h-screen w-full flex justify-center items-center">
-      <CreateTask/>
-    </div>
-  )
+  return <div></div>;
 }
