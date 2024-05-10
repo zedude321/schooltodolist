5979dffacd89b21a5cc06a8990123bb326e01aeb
Temuulen Saikhanbayar <temuulensaikhanbayar31@gmail.com>
2024-05-10 20:22:54 +0800

    homepage commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
+import CreateTeam from "@/components/create-team";
+import CreateTask from "@/components/create-task";
+
 export default function Home() {
-  return <div></div>;
+  return(
+    <div className="h-screen w-full flex justify-center items-center">
+      <CreateTask/>
+    </div>
+  )
 }
